23099e497ddae633ab4d5f387e7c720d3c603b9b
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-04-16 02:33:53 +0900

    update sketch

--- tests/sketch.rs
+++ tests/sketch.rs
@@ -1,28 +1,30 @@
 use try_blocks::try_blocks;
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
 #[try_blocks]
-pub fn smoke_free_fn() -> Result<(), Box<dyn std::error::Error>> {
+pub fn smoke_free_fn() -> Result<()> {
     try {
         let _ = foo()?;
 
-        let _: Result<_, Box<dyn std::error::Error>> = try {
+        let _: Result<_> = try {
             foo()?;
             42
         };
 
-        let _: Result<_, Box<dyn std::error::Error>> = try {
+        let _: Result<_> = try {
             foo()?;
         };
     }
 }
 
 #[try_blocks]
-pub async fn smoke_async_fn() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn smoke_async_fn() -> Result<()> {
     try {
         let _ = foo()?;
 
         (async {
-            let _: Result<_, Box<dyn std::error::Error>> = try {
+            let _: Result<_> = try {
                 bar().await?;
 
                 (async {}).await;
@@ -38,11 +40,11 @@ pub struct X;
 
 impl X {
     #[try_blocks]
-    pub fn smoke_method(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn smoke_method(&self) -> Result<()> {
         try {
             let _ = foo()?;
 
-            let _: Result<_, Box<dyn std::error::Error>> = try {
+            let _: Result<_> = try {
                 foo()?;
                 42
             };
@@ -51,15 +53,24 @@ impl X {
 }
 
 #[try_blocks]
-pub fn try_op_described_outside_of_try_block() -> Result<(), Box<dyn std::error::Error>> {
+impl X {
+    pub fn smoke_item_impl(&self) -> Result<()> {
+        try {
+            let _ = foo()?;
+        }
+    }
+}
+
+#[try_blocks]
+pub fn try_op_described_outside_of_try_block() -> Result<()> {
     let _ = foo()?;
     Ok(())
 }
 
-fn foo() -> std::io::Result<i32> {
+fn foo() -> Result<i32> {
     Ok(0)
 }
 
-async fn bar() -> std::io::Result<i32> {
+async fn bar() -> Result<i32> {
     Ok(0)
 }
